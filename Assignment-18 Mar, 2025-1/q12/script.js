def839c69f73b93fe9de55ccdae797b1aaedd3cf
Tejveer1453 <101708936+Tejveer1453@users.noreply.github.com>
2025-03-23 21:32:36 +0530

    Add files via upload

---
+++ Assignment-18 Mar, 2025-1/q12/script.js
@@ -0,0 +1,8 @@
+function toggleVisibility() {
+  const hiddenBox = document.getElementById("hiddenBox");
+  if (hiddenBox.style.display === "none") {
+    hiddenBox.style.display = "block";
+  } else {
+    hiddenBox.style.display = "none";
+  }
+}
